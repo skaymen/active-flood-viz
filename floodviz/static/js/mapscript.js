199ed8b853d0bbb367e51ca9b71d7358dde2b2a7
skaymen <skaymen@usgs.gov>
2017-06-26 10:00:57 -0500

    finished implementing suggested changes in second pull request

--- floodviz/static/js/mapscript.js
+++ floodviz/static/js/mapscript.js
@@ -1,7 +1,3 @@
-/**
- * Created by skaymen on 6/23/2017.
- */
-//document.getElementsByClassName("gages").setAttribute("r", 50);
 
 $('.gages').attr("r", 3).attr("fill", "red");
 $('.cities').attr("r", 3).attr("fill", "blue");

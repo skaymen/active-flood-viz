b3ba50053c52e8a80f93f307d3d445853e8e8b48
skaymen <skaymen@usgs.gov>
2017-08-08 13:53:35 -0500

    Switch to linear scale

--- floodviz/static/js/hydrograph.js
+++ floodviz/static/js/hydrograph.js
@@ -51,7 +51,7 @@ var hydromodule = function (options) {
 		});
 	// Set the ranges
 	var scaleX = d3.scaleTime().range([0, width]);
-	var scaleY = d3.scaleLog().range([height, 0]);
+	var scaleY = d3.scaleLinear().range([height, 0]);
 
 	// Google Analytics Boolean Trackers
 	var hydro_moused_over = false;
@@ -198,7 +198,7 @@ var hydromodule = function (options) {
 		// Add the Y Axis
 		svg.append('g')
 			.attr('class', 'axis')
-			.call(d3.axisLeft(scaleY).ticks(100, '.0f').tickSizeOuter(0));
+			.call(d3.axisLeft(scaleY).ticks(10, '.0f').tickSizeOuter(0));
 
 		// Tooltip
 		hydrotip = svg.append('g')
